9f15567653d1c038ffc1d5b8551419ea5619be12
Hùng Thanh <thanhhung180498@gmail.com>
2023-04-14 00:07:05 +0700

    Add events to form Change Password

--- ZiCoffee/DesktopApp/GUI/FormChangePassword.Designer.cs
+++ ZiCoffee/DesktopApp/GUI/FormChangePassword.Designer.cs
@@ -30,11 +30,11 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formChangePassword));
             this.txbOldPassword = new System.Windows.Forms.TextBox();
-            this.picEye = new System.Windows.Forms.PictureBox();
+            this.picEyeOld = new System.Windows.Forms.PictureBox();
             this.txbNewPassword = new System.Windows.Forms.TextBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.picEyeNew = new System.Windows.Forms.PictureBox();
             this.txbReenterPassword = new System.Windows.Forms.TextBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.picEyeReenter = new System.Windows.Forms.PictureBox();
             this.btnChange = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
             this.pnlHeader = new System.Windows.Forms.Panel();
@@ -45,9 +45,12 @@
             this.lbOldPassword = new System.Windows.Forms.Label();
             this.lbNewPassword = new System.Windows.Forms.Label();
             this.lbReEnter = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.picEye)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.lbCommentNewPassword = new System.Windows.Forms.Label();
+            this.lbCommentReenterPassword = new System.Windows.Forms.Label();
+            this.lbCommentOldPassword = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picEyeOld)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picEyeNew)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picEyeReenter)).BeginInit();
             this.pnlHeader.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.picMinimize)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picClose)).BeginInit();
@@ -64,21 +67,22 @@
             this.txbOldPassword.Text = "Password";
             this.txbOldPassword.UseSystemPasswordChar = true;
             // 
-            // picEye
+            // picEyeOld
             // 
-            this.picEye.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.picEye.Image = global::DesktopApp.Properties.Resources.show;
-            this.picEye.Location = new System.Drawing.Point(413, 89);
-            this.picEye.Name = "picEye";
-            this.picEye.Size = new System.Drawing.Size(30, 30);
-            this.picEye.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.picEye.TabIndex = 6;
-            this.picEye.TabStop = false;
+            this.picEyeOld.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.picEyeOld.Image = global::DesktopApp.Properties.Resources.show;
+            this.picEyeOld.Location = new System.Drawing.Point(413, 89);
+            this.picEyeOld.Name = "picEyeOld";
+            this.picEyeOld.Size = new System.Drawing.Size(30, 30);
+            this.picEyeOld.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picEyeOld.TabIndex = 6;
+            this.picEyeOld.TabStop = false;
+            this.picEyeOld.Click += new System.EventHandler(this.picEyeOld_Click);
             // 
             // txbNewPassword
             // 
             this.txbNewPassword.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txbNewPassword.Location = new System.Drawing.Point(57, 148);
+            this.txbNewPassword.Location = new System.Drawing.Point(57, 174);
             this.txbNewPassword.MaxLength = 20;
             this.txbNewPassword.Name = "txbNewPassword";
             this.txbNewPassword.Size = new System.Drawing.Size(350, 30);
@@ -86,21 +90,22 @@
             this.txbNewPassword.Text = "Password";
             this.txbNewPassword.UseSystemPasswordChar = true;
             // 
-            // pictureBox1
+            // picEyeNew
             // 
-            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.pictureBox1.Image = global::DesktopApp.Properties.Resources.show;
-            this.pictureBox1.Location = new System.Drawing.Point(413, 148);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(30, 30);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 9;
-            this.pictureBox1.TabStop = false;
+            this.picEyeNew.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.picEyeNew.Image = global::DesktopApp.Properties.Resources.show;
+            this.picEyeNew.Location = new System.Drawing.Point(413, 174);
+            this.picEyeNew.Name = "picEyeNew";
+            this.picEyeNew.Size = new System.Drawing.Size(30, 30);
+            this.picEyeNew.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picEyeNew.TabIndex = 9;
+            this.picEyeNew.TabStop = false;
+            this.picEyeNew.Click += new System.EventHandler(this.picEyeNew_Click);
             // 
             // txbReenterPassword
             // 
             this.txbReenterPassword.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txbReenterPassword.Location = new System.Drawing.Point(57, 207);
+            this.txbReenterPassword.Location = new System.Drawing.Point(57, 262);
             this.txbReenterPassword.MaxLength = 20;
             this.txbReenterPassword.Name = "txbReenterPassword";
             this.txbReenterPassword.Size = new System.Drawing.Size(350, 30);
@@ -108,16 +113,17 @@
             this.txbReenterPassword.Text = "Password";
             this.txbReenterPassword.UseSystemPasswordChar = true;
             // 
-            // pictureBox3
+            // picEyeReenter
             // 
-            this.pictureBox3.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.pictureBox3.Image = global::DesktopApp.Properties.Resources.show;
-            this.pictureBox3.Location = new System.Drawing.Point(413, 207);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(30, 30);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox3.TabIndex = 12;
-            this.pictureBox3.TabStop = false;
+            this.picEyeReenter.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.picEyeReenter.Image = global::DesktopApp.Properties.Resources.show;
+            this.picEyeReenter.Location = new System.Drawing.Point(413, 262);
+            this.picEyeReenter.Name = "picEyeReenter";
+            this.picEyeReenter.Size = new System.Drawing.Size(30, 30);
+            this.picEyeReenter.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picEyeReenter.TabIndex = 12;
+            this.picEyeReenter.TabStop = false;
+            this.picEyeReenter.Click += new System.EventHandler(this.picEyeReenter_Click);
             // 
             // btnChange
             // 
@@ -126,12 +132,13 @@
             this.btnChange.FlatAppearance.BorderSize = 3;
             this.btnChange.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnChange.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnChange.Location = new System.Drawing.Point(57, 243);
+            this.btnChange.Location = new System.Drawing.Point(57, 328);
             this.btnChange.Name = "btnChange";
             this.btnChange.Size = new System.Drawing.Size(180, 50);
             this.btnChange.TabIndex = 4;
             this.btnChange.Text = "Change";
             this.btnChange.UseVisualStyleBackColor = true;
+            this.btnChange.Click += new System.EventHandler(this.btnChange_Click);
             // 
             // btnCancel
             // 
@@ -140,12 +147,13 @@
             this.btnCancel.FlatAppearance.BorderSize = 3;
             this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnCancel.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCancel.Location = new System.Drawing.Point(263, 243);
+            this.btnCancel.Location = new System.Drawing.Point(263, 328);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(180, 50);
             this.btnCancel.TabIndex = 5;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.picClose_Click);
             // 
             // pnlHeader
             // 
@@ -168,6 +176,7 @@
             this.picMinimize.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.picMinimize.TabIndex = 1;
             this.picMinimize.TabStop = false;
+            this.picMinimize.Click += new System.EventHandler(this.picMinimize_Click);
             // 
             // picClose
             // 
@@ -179,6 +188,7 @@
             this.picClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.picClose.TabIndex = 0;
             this.picClose.TabStop = false;
+            this.picClose.Click += new System.EventHandler(this.picClose_Click);
             // 
             // pnlLeft
             // 
@@ -186,7 +196,7 @@
             this.pnlLeft.Location = new System.Drawing.Point(0, 50);
             this.pnlLeft.Margin = new System.Windows.Forms.Padding(4);
             this.pnlLeft.Name = "pnlLeft";
-            this.pnlLeft.Size = new System.Drawing.Size(50, 280);
+            this.pnlLeft.Size = new System.Drawing.Size(50, 360);
             this.pnlLeft.TabIndex = 0;
             // 
             // pnlRight
@@ -195,7 +205,7 @@
             this.pnlRight.Location = new System.Drawing.Point(450, 50);
             this.pnlRight.Margin = new System.Windows.Forms.Padding(4);
             this.pnlRight.Name = "pnlRight";
-            this.pnlRight.Size = new System.Drawing.Size(50, 280);
+            this.pnlRight.Size = new System.Drawing.Size(50, 360);
             this.pnlRight.TabIndex = 0;
             // 
             // lbOldPassword
@@ -212,7 +222,7 @@
             // 
             this.lbNewPassword.AutoSize = true;
             this.lbNewPassword.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbNewPassword.Location = new System.Drawing.Point(57, 122);
+            this.lbNewPassword.Location = new System.Drawing.Point(57, 148);
             this.lbNewPassword.Name = "lbNewPassword";
             this.lbNewPassword.Size = new System.Drawing.Size(143, 23);
             this.lbNewPassword.TabIndex = 0;
@@ -222,18 +232,57 @@
             // 
             this.lbReEnter.AutoSize = true;
             this.lbReEnter.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbReEnter.Location = new System.Drawing.Point(57, 181);
+            this.lbReEnter.Location = new System.Drawing.Point(57, 236);
             this.lbReEnter.Name = "lbReEnter";
             this.lbReEnter.Size = new System.Drawing.Size(181, 23);
             this.lbReEnter.TabIndex = 0;
             this.lbReEnter.Text = "Re-enter Password";
+            // 
+            // lbCommentNewPassword
+            // 
+            this.lbCommentNewPassword.AutoSize = true;
+            this.lbCommentNewPassword.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCommentNewPassword.ForeColor = System.Drawing.Color.Red;
+            this.lbCommentNewPassword.Location = new System.Drawing.Point(57, 207);
+            this.lbCommentNewPassword.Name = "lbCommentNewPassword";
+            this.lbCommentNewPassword.Size = new System.Drawing.Size(215, 19);
+            this.lbCommentNewPassword.TabIndex = 0;
+            this.lbCommentNewPassword.Text = "New password is not valid !!!";
+            this.lbCommentNewPassword.Visible = false;
+            // 
+            // lbCommentReenterPassword
+            // 
+            this.lbCommentReenterPassword.AutoSize = true;
+            this.lbCommentReenterPassword.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCommentReenterPassword.ForeColor = System.Drawing.Color.Red;
+            this.lbCommentReenterPassword.Location = new System.Drawing.Point(57, 295);
+            this.lbCommentReenterPassword.Name = "lbCommentReenterPassword";
+            this.lbCommentReenterPassword.Size = new System.Drawing.Size(247, 19);
+            this.lbCommentReenterPassword.TabIndex = 0;
+            this.lbCommentReenterPassword.Text = "Re-enter password is not valid !!!";
+            this.lbCommentReenterPassword.Visible = false;
+            // 
+            // lbCommentOldPassword
+            // 
+            this.lbCommentOldPassword.AutoSize = true;
+            this.lbCommentOldPassword.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCommentOldPassword.ForeColor = System.Drawing.Color.Red;
+            this.lbCommentOldPassword.Location = new System.Drawing.Point(57, 122);
+            this.lbCommentOldPassword.Name = "lbCommentOldPassword";
+            this.lbCommentOldPassword.Size = new System.Drawing.Size(208, 19);
+            this.lbCommentOldPassword.TabIndex = 0;
+            this.lbCommentOldPassword.Text = "Old password is not valid !!!";
+            this.lbCommentOldPassword.Visible = false;
             // 
             // formChangePassword
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(49)))), ((int)(((byte)(61)))));
-            this.ClientSize = new System.Drawing.Size(500, 330);
+            this.ClientSize = new System.Drawing.Size(500, 410);
+            this.Controls.Add(this.lbCommentOldPassword);
+            this.Controls.Add(this.lbCommentReenterPassword);
+            this.Controls.Add(this.lbCommentNewPassword);
             this.Controls.Add(this.lbReEnter);
             this.Controls.Add(this.lbNewPassword);
             this.Controls.Add(this.lbOldPassword);
@@ -243,22 +292,22 @@
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnChange);
             this.Controls.Add(this.txbReenterPassword);
-            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.picEyeReenter);
             this.Controls.Add(this.txbNewPassword);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.picEyeNew);
             this.Controls.Add(this.txbOldPassword);
-            this.Controls.Add(this.picEye);
+            this.Controls.Add(this.picEyeOld);
             this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ForeColor = System.Drawing.Color.White;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "formChangePassword";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FormChangePasswork";
-            ((System.ComponentModel.ISupportInitialize)(this.picEye)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picEyeOld)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picEyeNew)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picEyeReenter)).EndInit();
             this.pnlHeader.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.picMinimize)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picClose)).EndInit();
@@ -270,11 +319,11 @@
         #endregion
 
         private System.Windows.Forms.TextBox txbOldPassword;
-        private System.Windows.Forms.PictureBox picEye;
+        private System.Windows.Forms.PictureBox picEyeOld;
         private System.Windows.Forms.TextBox txbNewPassword;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox picEyeNew;
         private System.Windows.Forms.TextBox txbReenterPassword;
-        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox picEyeReenter;
         private System.Windows.Forms.Button btnChange;
         private System.Windows.Forms.Button btnCancel;
         private System.Windows.Forms.Panel pnlHeader;
@@ -285,5 +334,8 @@
         private System.Windows.Forms.Label lbOldPassword;
         private System.Windows.Forms.Label lbNewPassword;
         private System.Windows.Forms.Label lbReEnter;
+        private System.Windows.Forms.Label lbCommentNewPassword;
+        private System.Windows.Forms.Label lbCommentReenterPassword;
+        private System.Windows.Forms.Label lbCommentOldPassword;
     }
 }
