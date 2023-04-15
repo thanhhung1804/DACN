f7c49a7f130190077a3e4aacdc6855585b21589c
Hùng Thanh <thanhhung180498@gmail.com>
2023-04-16 01:20:48 +0700

    Design Order form.

--- ZiCoffee/DesktopApp/GUI/FormOrder.cs
+++ ZiCoffee/DesktopApp/GUI/FormOrder.cs
@@ -10,11 +10,40 @@ using System.Windows.Forms;
 
 namespace DesktopApp.GUI
 {
-    public partial class FormOrder : Form
+    public partial class formOrder : Form
     {
-        public FormOrder()
+        public formOrder()
         {
             InitializeComponent();
+        }
+
+        private void picMinimize_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void picMaximize_Click(object sender, EventArgs e)
+        {
+            FormWindowState state = FormWindowState.Normal;
+            if (WindowState == FormWindowState.Normal)
+            {
+                state = FormWindowState.Maximized;
+            }
+            WindowState = state;
+        }
+
+        private void picClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnOrder_Click(object sender, EventArgs e)
+        {
+            //Udate table status to using if it is being empty status.
+            //If table is being empty status, it will create new bill.
+            //Update bill detail for that bill.
+            //Close form.
+            Close();
         }
     }
 }
