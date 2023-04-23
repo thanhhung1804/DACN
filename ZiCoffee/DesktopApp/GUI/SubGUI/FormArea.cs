2388730f866f590230282998f72529008f527d74
Hùng Thanh <thanhhung180498@gmail.com>
2023-04-23 21:56:12 +0700

    Design area form

--- ZiCoffee/DesktopApp/GUI/SubGUI/FormArea.cs
+++ ZiCoffee/DesktopApp/GUI/SubGUI/FormArea.cs
@@ -16,5 +16,30 @@ namespace DesktopApp.GUI.SubGUI
         {
             InitializeComponent();
         }
+
+        private void formArea_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void formArea_SizeChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picNew_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDone_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
