1854c80e988d6ed5074ee5e6af0d6d1e058f9520
Hùng Thanh <thanhhung180498@gmail.com>
2023-04-23 23:48:59 +0700

    Design table form

--- ZiCoffee/DesktopApp/GUI/SubGUI/FormTable.cs
+++ ZiCoffee/DesktopApp/GUI/SubGUI/FormTable.cs
@@ -16,5 +16,55 @@ namespace DesktopApp.GUI.SubGUI
         {
             InitializeComponent();
         }
+
+        private void formTable_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void formTable_SizeChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txbSearch_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void picSearch_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picNew_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbAreaFilter_SelectedValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbStatusFilter_SelectedValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picClose_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDone_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
