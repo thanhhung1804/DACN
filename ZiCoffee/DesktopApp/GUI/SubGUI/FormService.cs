5cbd710a8ccca372187c3afd9610747a512149e5
Hùng Thanh <thanhhung180498@gmail.com>
2023-04-24 00:14:12 +0700

    Design service form

--- ZiCoffee/DesktopApp/GUI/SubGUI/FormService.cs
+++ ZiCoffee/DesktopApp/GUI/SubGUI/FormService.cs
@@ -16,5 +16,60 @@ namespace DesktopApp.GUI.SubGUI
         {
             InitializeComponent();
         }
+
+        private void formService_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void formService_SizeChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txbSearch_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void picSearch_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picNew_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbCategoryFilter_SelectedValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbStatusFilter_SelectedValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picClose_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDone_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picImage_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
