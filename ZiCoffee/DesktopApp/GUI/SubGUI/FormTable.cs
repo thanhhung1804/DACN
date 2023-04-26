f0f9032eddba8730b433bff19362eca524e968f2
Hùng Thanh <thanhhung180498@gmail.com>
2023-04-26 23:38:22 +0700

    Add events for table form

--- ZiCoffee/DesktopApp/GUI/SubGUI/FormTable.cs
+++ ZiCoffee/DesktopApp/GUI/SubGUI/FormTable.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,6 +13,19 @@ namespace DesktopApp.GUI.SubGUI
 {
     public partial class formTable : Form
     {
+        #region Form Round Corners
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect,     // x-coordinate of upper-left corner
+            int nTopRect,      // y-coordinate of upper-left corner
+            int nRightRect,    // x-coordinate of lower-right corner
+            int nBottomRect,   // y-coordinate of lower-right corner
+            int nWidthEllipse, // height of ellipse
+            int nHeightEllipse // width of ellipse
+        );
+        #endregion
+
         public formTable()
         {
             InitializeComponent();
@@ -19,52 +33,73 @@ namespace DesktopApp.GUI.SubGUI
 
         private void formTable_Load(object sender, EventArgs e)
         {
-
+            Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
+            //First load data
         }
 
         private void formTable_SizeChanged(object sender, EventArgs e)
         {
-
+            Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
         }
 
         private void txbSearch_KeyPress(object sender, KeyPressEventArgs e)
         {
-
+            int enterKeycode = 13;
+            if (e.KeyChar == enterKeycode)
+            {
+                //reload data
+            }
         }
 
         private void picSearch_Click(object sender, EventArgs e)
         {
-
+            //Reload data
         }
 
         private void picNew_Click(object sender, EventArgs e)
         {
-
+            pnlDetail.Visible = true;
+            foreach (Control control in pnlDetail.Controls)
+            {
+                if (control is TextBox)
+                {
+                    ((TextBox)control).Text = string.Empty;
+                }
+            }
+            rtxbDescription.Text = string.Empty;
         }
 
         private void picDelete_Click(object sender, EventArgs e)
         {
-
+            //check selected item
+            //delete record in database
+            //Notify
+            //reload data
+            pnlDetail.Visible = false;
         }
 
         private void cbAreaFilter_SelectedValueChanged(object sender, EventArgs e)
         {
-
+            //reload data
         }
 
         private void cbStatusFilter_SelectedValueChanged(object sender, EventArgs e)
         {
-
+            //reload data
         }
 
         private void picClose_Click(object sender, EventArgs e)
         {
-
+            pnlDetail.Visible = false;
         }
 
         private void btnDone_Click(object sender, EventArgs e)
         {
-
+            //valid datafield
+            //insert or update data
+            //Notify
+            //reload data
+            pnlDetail.Visible = false;
         }
     }
 }
