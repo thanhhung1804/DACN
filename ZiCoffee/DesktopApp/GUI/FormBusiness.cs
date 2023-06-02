e410eac6e6f88bd671b89a16ed334ee6156ca544
Hùng Thanh <thanhhung180498@gmail.com>
2023-06-03 04:51:00 +0700

    Support move & merge table

--- ZiCoffee/DesktopApp/GUI/FormBusiness.cs
+++ ZiCoffee/DesktopApp/GUI/FormBusiness.cs
@@ -1,6 +1,7 @@
 ﻿using DesktopApp.Common;
 using DesktopApp.DAO;
 using DesktopApp.DTO;
+using DesktopApp.Model;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -39,6 +40,8 @@ namespace DesktopApp.GUI
         private AreaDTO currentSelectedArea;
         private TableDTO currentSelectedTable;
         private UserDTO currentSelectedUser;
+
+        private Button draggedButton;
 
         public formBusiness()
         {
@@ -135,6 +138,13 @@ namespace DesktopApp.GUI
                 }
                 btnTable.Tag = table;
                 btnTable.Click += btnTable_Click;
+
+                btnTable.AllowDrop = true;
+                btnTable.MouseDown += btnTable_MouseDown;
+                btnTable.MouseMove += btnTable_MouseMove;
+                btnTable.DragEnter += btnTable_DragEnter;
+                btnTable.DragOver += btnTable_DragOver;
+                btnTable.DragDrop += btnTable_DragDrop;
                 fpnlTable.Controls.Add(btnTable);
             }
             LoadFooter();
@@ -152,6 +162,152 @@ namespace DesktopApp.GUI
             {
                 btnLockTable.Text = "Lock";
             }
+        }
+
+        private void btnTable_MouseDown(object sender, MouseEventArgs e)
+        {
+            btnTable_Click(sender, new EventArgs());
+            Button originButton = (Button)sender;
+            TableDTO originTable = originButton.Tag as TableDTO;
+
+            if (originTable.Status == TableStatus.Ready || originTable.Status == TableStatus.Pending)
+            {
+                return;
+            }
+
+            if (e.Button != MouseButtons.Left)
+            {
+                return;
+            }
+                
+            draggedButton = originButton;
+            draggedButton.DoDragDrop(draggedButton, DragDropEffects.Move);
+        }
+
+        private void btnTable_MouseMove(object sender, MouseEventArgs e)
+        {
+            Button originButton = (Button)sender;
+            TableDTO originTable = originButton.Tag as TableDTO;
+
+            if (originTable.Status == TableStatus.Ready || originTable.Status == TableStatus.Pending)
+            {
+                return;
+            }
+
+            if (e.Button != MouseButtons.Left)
+            {
+                return;
+            }
+
+            draggedButton = originButton;
+            draggedButton.DoDragDrop(draggedButton, DragDropEffects.Move);
+        }
+
+        private void btnTable_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(typeof(Button)))
+            {
+                e.Effect = DragDropEffects.Move;
+            }
+        }
+
+        private void btnTable_DragOver(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Move;
+        }
+
+        private void btnTable_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(typeof(Button)))
+            {
+                Button originButton = (Button)e.Data.GetData(typeof(Button));
+                TableDTO originTable = originButton.Tag as TableDTO;
+
+                Button targetButton = (Button)sender;
+                TableDTO targetTable = targetButton.Tag as TableDTO;
+
+                if (originTable == targetTable)
+                {
+                    return;
+                }
+
+                switch (targetTable.Status)
+                {
+                    case TableStatus.Ready:
+                        MoveTable(originTable, targetTable);
+                        break;
+                    case TableStatus.Using:
+                        MergeTable(originTable, targetTable);
+                        break;
+                    default: break;
+                }
+            }
+        }
+
+        private void MoveTable(TableDTO originTable, TableDTO targetTable)
+        {
+            var result = MessageBox.Show(
+                text: string.Format("Are you sure you want to move {0} to {1}?", originTable.Name, targetTable.Name), 
+                caption: "Confirmation", 
+                buttons: MessageBoxButtons.OKCancel, 
+                icon: MessageBoxIcon.Question
+            );
+
+            if (result == DialogResult.Cancel)
+            {
+                return;
+            }
+
+            BillDTO originBill = new BillDAO().GetUnpaidBillByTable(tableId: originTable.TableId);
+            new BillDAO().UpdateTableId(billId: originBill.BillId, tableId: targetTable.TableId);
+            new TableDAO().ChangeStatus(tableId: originTable.TableId, status: TableStatus.Ready);
+            new TableDAO().ChangeStatus(tableId: targetTable.TableId, status: TableStatus.Using);
+            LoadTable();
+        }
+
+        private void MergeTable(TableDTO originTable, TableDTO targetTable)
+        {
+            var result = MessageBox.Show(
+                text: string.Format("Are you sure you want to merge {0} into {1}?", originTable.Name, targetTable.Name),
+                caption: "Confirmation",
+                buttons: MessageBoxButtons.OKCancel,
+                icon: MessageBoxIcon.Question
+            );
+
+            if (result == DialogResult.Cancel)
+            {
+                return;
+            }
+
+            BillDTO originBill = new BillDAO().GetUnpaidBillByTable(tableId: originTable.TableId);
+            BillDTO targetBill = new BillDAO().GetUnpaidBillByTable(tableId: targetTable.TableId);
+            List<BillDetailDTO> originBillDetails = new BillDetailDAO().GetBillDetails(billId: originBill.BillId);
+            List<BillDetailDTO> targetBillDetails = new BillDetailDAO().GetBillDetails(billId: targetBill.BillId);
+            foreach (BillDetailDTO originItem in originBillDetails)
+            {
+                BillDetailDTO targetItem = targetBillDetails.Find(item => item.ServiceId == originItem.ServiceId);
+                if (targetItem != null)
+                {
+                    targetItem.Quantity += originItem.Quantity;
+                    targetItem.Amount += originItem.Amount;
+                }
+                else
+                {
+                    originItem.BillId = targetBill.BillId;
+                    targetBillDetails.Add(originItem);
+                }
+            }
+            new BillDetailDAO().DeleteBillDetails(billId: targetBill.BillId);
+            float total = 0;
+            foreach (BillDetailDTO targetItem in targetBillDetails)
+            {
+                new BillDetailDAO().CreateBillDetails(billDetail: targetItem);
+                total += targetItem.Amount;
+            }
+            new BillDAO().UpdateTotal(billId: targetBill.BillId, total: total);
+            new BillDAO().Delete(billId: originBill.BillId);
+            new TableDAO().ChangeStatus(tableId: originTable.TableId, status: TableStatus.Ready);
+            LoadTable();
         }
 
         private void LoadFooter()
